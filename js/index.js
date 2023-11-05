b9357f12406a301e234bde09fbd4f327e03e050d
karthickop6 <karthickop6@gmail.com>
2023-11-05 10:15:57 +0530

    Fixed the blog and the rest of the pages

--- js/index.js
+++ js/index.js
@@ -129,6 +129,8 @@ gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#portfolio',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
 })
+
+
 $('#blog-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
@@ -143,6 +145,7 @@ gsap.to('#breaker-two',0,{display:"none",delay:2});
 gsap.to('#blog',0,{display:"block",delay:.7});
 gsap.to('#navigation-content',0,{display:'flex',delay:2});
 })
+
 $('#home-link').on('click',function(){
   gsap.to('#navigation-content',0,{display:"none",delay:.7});
   gsap.to('#navigation-content',0,{y:'-100%',delay:.7});
